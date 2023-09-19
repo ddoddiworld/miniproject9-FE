43408dbbd771e3348f6ec4cb00063404074735e3
Xeonxoo_99 <dustn49@gmail.com>
2023-09-19 19:32:07 +0900

    X버튼 추가 / 글씨색 변경

--- src/redux/components/Modal/LoginModal.jsx
+++ src/redux/components/Modal/LoginModal.jsx
@@ -1,43 +1,50 @@
-import React from "react";
-import styles from "./styles";
-import { usestate } from "react";
+import React from 'react';
+import styles from './styles';
+import { useState } from 'react';
 
-function LoginModal({ isOpen, closeModal }) {
-  const {
-    Modal,
-    ModalTitle,
-    ModalContents,
-    InputFild,
-    Input,
-    ModalLabel,
-    ModalBtn,
-    ModalP,
-    ModalLink,
-  } = styles;
+function LoginModal({ close }) {
+    const {
+        Modal,
+        ModalTitle,
+        ModalContents,
+        InputFild,
+        Input,
+        ModalLabel,
+        ModalBtn,
+        ModalP,
+        ModalLink,
+    } = styles;
 
-  return (
-    <>
-      <Modal>
-        <ModalTitle>로그인</ModalTitle>
-        <ModalContents>
-          <InputFild>
-            <ModalLabel> 이메일 </ModalLabel>
-            <ModalP center={false}></ModalP>
-            <Input />
-          </InputFild>
-          <InputFild>
-            <ModalLabel> 비밀번호 </ModalLabel>
-            <ModalP center={false}></ModalP>
-            <Input />
-          </InputFild>
-          <ModalBtn bgcolor="#ffa500">로그인</ModalBtn>
-          <ModalP>
-            회원이 아니신가요?<ModalLink>회원가입</ModalLink>
-          </ModalP>
-        </ModalContents>
-      </Modal>
-    </>
-  );
+    const [closeModal] = useState(true);
+
+    return (
+        <>
+            {closeModal && (
+                <Modal>
+                    <ModalTitle>
+                        로그인<button onClick={close}>x</button>
+                    </ModalTitle>
+                    <ModalContents>
+                        <InputFild>
+                            <ModalLabel> 이메일 </ModalLabel>
+                            <ModalP center={false}></ModalP>
+                            <Input />
+                        </InputFild>
+                        <InputFild>
+                            <ModalLabel> 비밀번호 </ModalLabel>
+                            <ModalP center={false}></ModalP>
+                            <Input />
+                        </InputFild>
+                        <ModalBtn bgcolor="#ffa500">로그인</ModalBtn>
+                        <ModalP>
+                            {/* 회원가입버튼을 누르면 로그인 모달창이 닫히고, 회원가입 모달창이 나오도록? */}
+                            회원이 아니신가요?<ModalLink>회원가입</ModalLink>
+                        </ModalP>
+                    </ModalContents>
+                </Modal>
+            )}
+        </>
+    );
 }
 
 export default LoginModal;
--- src/redux/components/Modal/SignUpModal.jsx
+++ src/redux/components/Modal/SignUpModal.jsx
@@ -1,7 +1,8 @@
 import React from 'react';
 import styles from './styles';
+import { useState } from 'react';
 
-function SignUpModal() {
+function SignUpModal({ close }) {
     const {
         Modal,
         ModalTitle,
@@ -15,29 +16,42 @@ function SignUpModal() {
         ModalLink,
     } = styles;
 
+    const [closeModal] = useState(true);
+    // <>
+    //     {closeModal && ()}
+    //     </>
     return (
-        <Modal>
-            <ModalTitle>회원가입</ModalTitle>
-            <ModalContents>
-                <InputFild>
-                    <ModalLabel margin="0 15px 0 0"> 이메일 </ModalLabel>
-                    <Input />
-                    <TestBtn center={false}> 중복검사 </TestBtn>
-                </InputFild>
-                <InputFild>
-                    <ModalLabel> 비밀번호 </ModalLabel>
-                    <Input />
-                </InputFild>
-                <InputFild>
-                    <ModalLabel> 비밀번호 확인</ModalLabel>
-                    <Input />
-                </InputFild>
-                <ModalBtn>회원가입 완료</ModalBtn>
-                <ModalP>
-                    회원이신가요?<ModalLink>로그인</ModalLink>
-                </ModalP>
-            </ModalContents>
-        </Modal>
+        <>
+            {closeModal && (
+                <Modal>
+                    <ModalTitle>
+                        회원가입<button onClick={close}>x</button>
+                    </ModalTitle>
+                    <ModalContents>
+                        <InputFild>
+                            <ModalLabel margin="0 15px 0 0">
+                                {' '}
+                                이메일{' '}
+                            </ModalLabel>
+                            <Input />
+                            <TestBtn center={false}> 중복검사 </TestBtn>
+                        </InputFild>
+                        <InputFild>
+                            <ModalLabel> 비밀번호 </ModalLabel>
+                            <Input />
+                        </InputFild>
+                        <InputFild>
+                            <ModalLabel> 비밀번호 확인</ModalLabel>
+                            <Input />
+                        </InputFild>
+                        <ModalBtn>회원가입 완료</ModalBtn>
+                        <ModalP>
+                            회원이신가요?<ModalLink>로그인</ModalLink>
+                        </ModalP>
+                    </ModalContents>
+                </Modal>
+            )}
+        </>
     );
 }
 
