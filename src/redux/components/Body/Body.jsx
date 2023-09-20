e61c4a25b3aaf3e8c41f8ebfe2cecc1d52edf932
Xeonxoo_99 <dustn49@gmail.com>
2023-09-20 15:25:46 +0900

    Cookie를 써보자

--- src/redux/components/Body/Body.jsx
+++ src/redux/components/Body/Body.jsx
@@ -1,123 +1,133 @@
-import React from "react";
-import styles from "./styles";
-import moon from "../images/moon2.png";
-import star from "../images/star2.png";
-import User from "../User/User";
-import LoginModal from "../Modal/LoginModal";
-import SignUpModal from "../Modal/SignUpModal";
-import WriteModal from "../Modal/WriteModal";
-import ViewModal from "../Modal/ViewModal";
-import { useState } from "react";
+import React from 'react';
+import styles from './styles';
+import moon from '../images/moon2.png';
+import star from '../images/star2.png';
+import User from '../User/User';
+import LoginModal from '../Modal/LoginModal';
+import SignUpModal from '../Modal/SignUpModal';
+import WriteModal from '../Modal/WriteModal';
+import ViewModal from '../Modal/ViewModal';
+import { useState } from 'react';
 
 function Body() {
-  /***********************
+    /***********************
     공통
   ***********************/
-  // 스타일
-  const {
-    Main,
-    MainWarp,
-    Title,
-    SubTitle,
-    StyledBtn,
-    Moon,
-    Star,
-    SideOverlay,
-  } = styles;
+    // 스타일
+    const {
+        Main,
+        MainWarp,
+        Title,
+        SubTitle,
+        StyledBtn,
+        Moon,
+        Star,
+        SideOverlay,
+    } = styles;
 
-  // 별 위치 position
-  const starts = [
-    { top: "19%", right: "20%", width: "100px" },
-    { top: "30%", right: "10%", width: "60px" },
-    { top: "43%", right: "12%", width: "80px" },
-    { top: "55%", right: "25%", width: "80px" },
-    { top: "65%", right: "10%", width: "120px" },
+    // 별 위치 position
+    const starts = [
+        { top: '19%', right: '20%', width: '100px' },
+        { top: '30%', right: '10%', width: '60px' },
+        { top: '43%', right: '12%', width: '80px' },
+        { top: '55%', right: '25%', width: '80px' },
+        { top: '65%', right: '10%', width: '120px' },
 
-    { top: "auto", left: "10%", width: "100px" },
-    { top: "22%", left: "32%", width: "60px" },
-    { top: "55%", left: "15%", width: "60px" },
-    { top: "38%", left: "25%", width: "80px" },
-    { top: "65%", left: "27%", width: "120px" },
-  ];
+        { top: 'auto', left: '10%', width: '100px' },
+        { top: '22%', left: '32%', width: '60px' },
+        { top: '55%', left: '15%', width: '60px' },
+        { top: '38%', left: '25%', width: '80px' },
+        { top: '65%', left: '27%', width: '120px' },
+    ];
 
-  // 오버레이
-  const [showOverlay, setshowOverlay] = useState(false);
+    // 오버레이
+    const [showOverlay, setshowOverlay] = useState(false);
 
-  // 모달창 닫기
-  const close = () => {
-    setshowOverlay(false);
-    setopenLoginModal(false);
-    setopenSignModal(false);
-    setOpenDuckdomModal(false);
-  };
+    // 모달창 닫기
+    const close = () => {
+        setshowOverlay(false);
+        setopenLoginModal(false);
+        setopenSignModal(false);
+        setOpenDuckdomModal(false);
+    };
 
-  /***********************
+    /***********************
     로그인 전
   ***********************/
-  // 로그인
-  const [openLoginModal, setopenLoginModal] = useState(false);
+    // 로그인
+    const [openLoginModal, setopenLoginModal] = useState(false);
 
-  const loginOpen = () => {
-    setshowOverlay(true);
-    setopenLoginModal(true);
-  };
+    const loginOpen = () => {
+        setshowOverlay(true);
+        setopenLoginModal(true);
+    };
 
-  // 회원가입
-  const [openSignModal, setopenSignModal] = useState(false);
+    // 회원가입
+    const [openSignModal, setopenSignModal] = useState(false);
 
-  const signOpen = () => {
-    setshowOverlay(true);
-    setopenSignModal(true);
-  };
+    const signOpen = () => {
+        setshowOverlay(true);
+        setopenSignModal(true);
+    };
 
-  /***********************
+    /***********************
     로그인 후
   ***********************/
-  // 덕담 나눠주기 (작성)
-  const [openDuckdomModal, setOpenDuckdomModal] = useState(false);
-  const giveDuckdom = () => {
-    setshowOverlay(true);
-    setOpenDuckdomModal(true);
-  };
+    // 덕담 나눠주기 (작성)
+    const [openDuckdomModal, setOpenDuckdomModal] = useState(false);
+    const giveDuckdom = () => {
+        setshowOverlay(true);
+        setOpenDuckdomModal(true);
+    };
 
-  // 별 추가 (보기)
-  const addStar = () => {
-    alert("별을 누르면 받은 글을 볼 수 있어요.");
-  };
+    // 별 추가 (보기)
+    const addStar = () => {
+        alert('별을 누르면 받은 글을 볼 수 있어요.');
+    };
 
-  return (
-    <>
-      {/* 로그인 안된 상태 */}
-      <Main>
-        <MainWarp>
-          <Title>토끼의 발자국</Title>
-          <SubTitle>고마운 마음을 담아 서로에게 덕담 한마디 어떨까요?</SubTitle>
-          {showOverlay && <SideOverlay onClick={close} />}
-          {openLoginModal && (
-            <>
-              <LoginModal close={close} signOpen={signOpen}></LoginModal>
-            </>
-          )}
-          {openSignModal && (
-            <>
-              <SignUpModal close={close} loginOpen={loginOpen}></SignUpModal>
-            </>
-          )}
-          <div>
-            <Moon src={moon} alt="moon"></Moon>
-            {starts.map((item, index) => {
-              return <Star src={star} key={index} {...item}></Star>;
-            })}
-          </div>
-          <StyledBtn size={"medium"} onClick={loginOpen}>
-            로그인
-          </StyledBtn>
-          <User></User>
-        </MainWarp>
-      </Main>
+    return (
+        <>
+            {/* 로그인 안된 상태 */}
+            <Main>
+                <MainWarp>
+                    <Title>토끼의 발자국</Title>
+                    <SubTitle>
+                        고마운 마음을 담아 서로에게 덕담 한마디 어떨까요?
+                    </SubTitle>
+                    {showOverlay && <SideOverlay onClick={close} />}
+                    {openLoginModal && (
+                        <>
+                            <LoginModal
+                                close={close}
+                                signOpen={signOpen}
+                            ></LoginModal>
+                        </>
+                    )}
+                    {openSignModal && (
+                        <>
+                            <SignUpModal
+                                close={close}
+                                loginOpen={loginOpen}
+                            ></SignUpModal>
+                        </>
+                    )}
+                    <div>
+                        <Moon src={moon} alt="moon"></Moon>
+                        {starts.map((item, index) => {
+                            return (
+                                <Star src={star} key={index} {...item}></Star>
+                            );
+                        })}
+                    </div>
+                    <StyledBtn size={'medium'} onClick={loginOpen}>
+                        로그인
+                    </StyledBtn>
+                    <User></User>
+                </MainWarp>
+            </Main>
 
-      {/* 로그인이 되어 있는 상태 */}
-      {/* <Main>
+            {/* 로그인이 되어 있는 상태 */}
+            {/* <Main>
         <MainWarp>
           <Title>토끼의 발자국</Title>
           <SubTitle>고마운 마음을 담아 서로에게 덕담 한마디 어떨까요?</SubTitle>
@@ -144,8 +154,8 @@ function Body() {
           <User></User>
         </MainWarp>
       </Main> */}
-    </>
-  );
+        </>
+    );
 }
 
 export default Body;
--- src/redux/components/Modal/LoginModal.jsx
+++ src/redux/components/Modal/LoginModal.jsx
@@ -1,119 +1,117 @@
-import React from "react";
-import styles from "./styles";
-import { useState, useEffect } from "react";
-import axios from "axios";
-import { getCookie, setCookie } from "../../../token/token";
+import React from 'react';
+import styles from './styles';
+import { useState, useEffect } from 'react';
+import axios from 'axios';
+import { getCookie, setCookie } from '../../../token/token';
 
 function LoginModal({ close, signOpen }) {
-  const {
-    Modal,
-    ModalTitle,
-    ModalContents,
-    InputFild,
-    Input,
-    ModalLabel,
-    ModalBtn,
-    ModalP,
-    ModalLink,
-    CloseBtn,
-  } = styles;
+    const {
+        Modal,
+        ModalTitle,
+        ModalContents,
+        InputFild,
+        Input,
+        ModalLabel,
+        ModalBtn,
+        ModalP,
+        ModalLink,
+        CloseBtn,
+    } = styles;
 
-  const [closeModal] = useState(true);
-  const [email, setEmail] = useState("");
-  const [password, setPassword] = useState("");
+    const [closeModal] = useState(true);
+    const [email, setEmail] = useState('');
+    const [password, setPassword] = useState('');
 
-  // 토큰 get
-  useEffect(() => {
-    const token = getCookie();
-    if (token) {
-      alert(token);
-    }
-  }, []);
+    const onChangeEmail = (e) => {
+        setEmail(e.target.value);
+    };
+    const onChangePassword = (e) => {
+        setPassword(e.target.value);
+    };
 
-  const user = async () => {
-    try {
-      const response = await axios.get("http://3.38.191.164/api/signin", {
-        headers: {
-          Authorization: `Bearer ${localStorage.getItem("token")}`,
-        },
-      });
-      console.log(response);
-    } catch (error) {
-      alert(error);
-    }
-  };
-
-  // 로그인 버튼 fn
-  const loginHandler = async () => {
-    try {
-      if (!email || !password) {
-        alert("이메일과 비밀번호를 입력해 주세요!");
-        return;
-      } else {
-        const response = await axios.post(
-          "http://54.180.87.103:4000/api/signin",
-          {
-            email,
-            password,
-          }
-        );
-        console.log(response.status);
-
-        // 로그인 성공
-        if (response.status === 200) {
-          setCookie(response.data.token);
-          user();
-          alert("[로그인 성공]\n안녕하세요! 좋은 하루 보내세요😄");
-          // navigate("/home");
+    // 토큰 get
+    useEffect(() => {
+        const token = getCookie();
+        if (token) {
+            alert(token);
         }
-      }
-    } catch (error) {
-      alert("[로그인 실패]\n앗!🥲 올바른 ID와 Password 를 입력해 주세요!");
-      console.error("로그인 실패! :", error.message);
-    }
-  };
+    }, []);
 
-  return (
-    <>
-      {closeModal && (
-        <Modal>
-          <ModalTitle>
-            로그인<CloseBtn onClick={close}>x</CloseBtn>
-          </ModalTitle>
-          <ModalContents>
-            <InputFild>
-              <ModalLabel> 이메일 </ModalLabel>
-              <ModalP center={false}></ModalP>
-              <Input value={email} onChange={(e) => setEmail(e.target.value)} />
-            </InputFild>
-            <InputFild>
-              <ModalLabel> 비밀번호 </ModalLabel>
-              <ModalP center={false}></ModalP>
-              <Input
-                value={password}
-                onChange={(e) => setPassword(e.target.value)}
-              />
-            </InputFild>
-            <ModalBtn bgcolor="#ffa500" onClick={loginHandler}>
-              로그인
-            </ModalBtn>
-            <ModalP>
-              {/* 회원가입버튼을 누르면 로그인 모달창이 닫히고, 회원가입 모달창이 나오도록? */}
-              회원이 아니신가요?
-              <ModalLink
-                onClick={() => {
-                  close();
-                  signOpen();
-                }}
-              >
-                회원가입
-              </ModalLink>
-            </ModalP>
-          </ModalContents>
-        </Modal>
-      )}
-    </>
-  );
+    // 로그인 버튼 fn
+    const loginHandler = async () => {
+        try {
+            if (!email || !password) {
+                alert('이메일과 비밀번호를 입력해 주세요!');
+                return;
+            } else {
+                const response = await axios.post(
+                    'http://54.180.87.103:4000/api/signin',
+                    {
+                        email,
+                        password,
+                    }
+                );
+                console.log(response.status);
+
+                // 로그인 성공
+                if (response.status === 200) {
+                    setCookie(response.data.token);
+                    alert('[로그인 성공]\n안녕하세요! 좋은 하루 보내세요😄');
+                    // navigate("/home");
+                }
+            }
+        } catch (error) {
+            alert(
+                '[로그인 실패]\n앗!🥲 올바른 ID와 Password 를 입력해 주세요!'
+            );
+            console.error('로그인 실패! :', error.message);
+        }
+    };
+
+    return (
+        <>
+            {closeModal && (
+                <Modal>
+                    <ModalTitle>
+                        로그인<CloseBtn onClick={close}>x</CloseBtn>
+                    </ModalTitle>
+                    <ModalContents>
+                        <InputFild>
+                            <ModalLabel> 이메일 </ModalLabel>
+                            <ModalP center={false}></ModalP>
+                            <Input
+                                value={email}
+                                onChange={(e) => setEmail(e.target.value)}
+                            />
+                        </InputFild>
+                        <InputFild>
+                            <ModalLabel> 비밀번호 </ModalLabel>
+                            <ModalP center={false}></ModalP>
+                            <Input
+                                value={password}
+                                onChange={(e) => setPassword(e.target.value)}
+                            />
+                        </InputFild>
+                        <ModalBtn bgcolor="#ffa500" onClick={loginHandler}>
+                            로그인
+                        </ModalBtn>
+                        <ModalP>
+                            {/* 회원가입버튼을 누르면 로그인 모달창이 닫히고, 회원가입 모달창이 나오도록? */}
+                            회원이 아니신가요?
+                            <ModalLink
+                                onClick={() => {
+                                    close();
+                                    signOpen();
+                                }}
+                            >
+                                회원가입
+                            </ModalLink>
+                        </ModalP>
+                    </ModalContents>
+                </Modal>
+            )}
+        </>
+    );
 }
 
 export default LoginModal;
